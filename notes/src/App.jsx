d782ec7c9a7f19ecc0d9d40a7a2877fb20a72697
ArunveerSidhu <sidhu.arunveer@gmail.com>
2024-12-31 23:48:55 +0530

    react setup

---
+++ notes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className="text-red-600 underline">
+        hello world
+      </h1>      
+    </>
+  )
+}
+
+export default App
