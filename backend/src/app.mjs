ef3b45fdc7dce67719269af75782a84f1e0378d2
ArunveerSidhu <sidhu.arunveer@gmail.com>
2025-01-02 02:16:30 +0530

    tested user routes with insomnia and fixed a few typos

--- backend/src/app.mjs
+++ backend/src/app.mjs
@@ -1,15 +1,18 @@
 import express from "express";
-const app = express();
 import dotenv from "dotenv";
 import connectDB from "../config/db.mjs";
+import userRoutes from "../routes/user.routes.mjs"
 
 dotenv.config();
 
+const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
 
 connectDB();
+
+app.use('/api/users', userRoutes);
 
 app.listen(port, ()=>{
   console.log(`Server is running on port ${port}`);
